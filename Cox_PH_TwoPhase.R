20f47def4f137baaf9435573d7cc76b416efcbc5
htzheng1018 <htzheng@umich.edu>
2024-07-30 15:12:57 -0400

    put libarary into sim_config function

--- Cox_PH_TwoPhase.R
+++ Cox_PH_TwoPhase.R
@@ -19,43 +19,19 @@ run_on_cluster(
     sim = new_sim()
     
     sim %<>% set_levels(
-      n = 50000,
+      n = c(100, 500, 1000, 5000, 10000),
       surv_time = list(
         "Exp" = list(surv_type = "Exponential", surv_params = 1.5e-2), # may be some problems
         "Gom" = list(surv_type = "Gompertz", surv_params = c(0.2138, 7e-8))
-      ),
-      error_meth = list(
-        "Add" = list(meth_type = "additive", meth_params = c(0, 359.1)),
-        "Mul" = list(meth_type = "multiplicative", meth_params = c(-0.2029, 0.637))
-      ),
-      error_relation = c("Classical", "Berkson")
+      )
     )
     
-    create_data = function(n, meth_type, meth_params, error_relation, surv_type, surv_params) {
+    create_data = function(n, surv_type, surv_params) {
       # X1
       X1 = rnorm(n = n, mean = 24.3, sd = 8.38)
       
       # X2
-      X2_true = rnorm(n = n, mean = 266.84, sd = 507.82)
-      if (error_relation == "Classical") {
-        # errors
-        if (meth_type == "additive") {
-          error = rnorm(n = n, mean = meth_params[1], sd = meth_params[2])
-          X2 = X2_true + error
-        } else if (meth_type == "multiplicative") {
-          error = rlnorm(n = n, meanlog = meth_params[1], sdlog = meth_params[2])
-          X2 = X2_true * error
-        }
-      } else if (error_relation == "Berkson") {
-        # errors
-        if (meth_type == "additive") {
-          error = rnorm(n = n, mean = meth_params[1], sd = meth_params[2])
-          X2 = X2_true - error
-        } else if (meth_type == "multiplicative") {
-          error = rlnorm(n = n, meanlog = meth_params[1], sdlog = meth_params[2])
-          X2 = X2_true / error
-        }
-      }
+      X2 = rnorm(n = n, mean = 266.84, sd = 507.82)
       
       # biomarker
       S = rtruncnorm(n = n, a = 0, b = 1, mean = 0.5, sd = 0.2) # truncated normal in (0, 1)
@@ -67,22 +43,22 @@ run_on_cluster(
       U = runif(n = n)
       if (surv_type == "Exponential") {
         lambda = surv_params
-        t = -log(U) / (lambda * exp(0.15*X1 + 0.001*X2_true + 5*S))
+        t = -log(U) / (lambda * exp(0.15*X1 + 0.001*X2 - 5*S))
       } else if (surv_type == "Gompertz") {
         alpha = surv_params[1]
         lambda = surv_params[2]
-        t = 1/alpha * log(1 - (alpha * log(U)) / (lambda * exp(0.15*X1 + 0.001*X2_true + 5*S)))
+        t = 1/alpha * log(1 - (alpha * log(U)) / (lambda * exp(0.15*X1 + 0.001*X2 - 5*S)))
       }
       
       # censore time
       U = runif(n = n)
       if (surv_type == "Exponential") {
         lambda = surv_params
-        C = - log(U) / (lambda * exp(0.15*X1 + 0.001*X2_true))
+        C = - log(U) / (lambda * exp(0.15*X1 + 0.001*X2))
       } else if (surv_type == "Gompertz") {
         alpha = surv_params[1]
         lambda = surv_params[2]
-        C = (- 1/lambda * log(U) * exp(0.15*X1 + 0.001*X2_true)) ^ (alpha)
+        C = (- 1/lambda * log(U) * exp(0.15*X1 + 0.001*X2)) ^ (alpha)
       }
       
       # delta
@@ -93,18 +69,18 @@ run_on_cluster(
       
       # two-phase indicator
       t0 = 200 # set the time of interest
-      prob_tmp = 1 / (1 + exp(0.15*X1 + 0.001*X2_true - 1))
+      prob_tmp = 1 / (1 + exp(0.15*X1 + 0.001*X2 - 1))
       Z = delta*I(Y <= t0) + (1 - delta*I(Y <= t0)) * rbinom(n = n, size = 1, prob = prob_tmp)
       
       data = data.frame("Y" = Y, "delta" = delta, "S" = S, "X1" = X1, "X2" = X2, "Z" = Z)
       return(data)
     }
     
-    sim %<>% set_config(num_sim = 1000, n_cores = 4, seed = 1018)
+    sim %<>% set_config(num_sim = 1000, n_cores = 4, seed = 1018,
+                        packages = c("SimEngine", "survival", "parallel", "truncnorm", "survival"))
     
     sim %<>% set_script(function() {
-      library(survival)
-      dat_phaseOne = create_data(L$n, L$error_meth$meth_type, L$error_meth$meth_params, L$error_relation, L$surv_time$surv_type, L$surv_time$surv_params)
+      dat_phaseOne = create_data(L$n, L$surv_time$surv_type, L$surv_time$surv_params)
       dat_phaseTwo = dat_phaseOne %>%
         filter(Z == 1) # use phase two data
       model = coxph(Surv(Y, delta) ~ X1 + X2 + S, data = dat_phaseTwo)
@@ -117,7 +93,7 @@ run_on_cluster(
         "beta_S_se" = summary(model)$coef["S", "se(coef)"],
         "X1_pctg" = (model$coef["X1"] - 0.15) / 0.15 * 100,
         "X2_pctg" = (model$coef["X2"] - 0.001) / 0.001 *100,
-        "S_pctg" = (model$coef["S"] - 5) / 5 *100,
+        "S_pctg" = (model$coef["S"] + 5) / (-5) *100,
         ".complex" = list(
           "model" = model,
           "cov_mtx" = vcov(model)
@@ -140,10 +116,10 @@ run_on_cluster(
     bias_beta = sim %>% summarize(
       list(stat = "bias", estimate = "beta_X1_hat", truth = 0.15, name = "bias_beta_X1"),
       list(stat = "bias", estimate = "beta_X2_hat", truth = 0.001, name = "bias_beta_X2"),
-      list(stat = "bias", estimate = "beta_S_hat", truth = 0.001, name = "bias_beta_S"),
+      list(stat = "bias", estimate = "beta_S_hat", truth = -5, name = "bias_beta_S"),
       list(stat = "coverage", estimate = "beta_X1_hat", se = "beta_X1_se", truth = 0.15, name = "cov_beta_X1"),
       list(stat = "coverage", estimate = "beta_X2_hat", se = "beta_X2_se", truth = 0.001, name = "cov_beta_X2"),
-      list(stat = "coverage", estimate = "beta_S_hat", se = "beta_S_se", truth = 5, name = "cov_beta_S")
+      list(stat = "coverage", estimate = "beta_S_hat", se = "beta_S_se", truth = -5, name = "cov_beta_S")
     )
     # bias percentage
     bias_beta_pct = sim %>% summarize(
