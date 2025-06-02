5cf5f25e84ddfa0f6c3d1762d0a1751c6426ba2f
htzheng1018 <htzheng00@gmail.com>
2025-06-02 15:12:46 -0400

    script for saving

--- R/script.R
+++ R/script.R
@@ -1,7 +1,14 @@
-
+library(kableExtra)
 
 
 
 # save files
 kable(bias_Q_pct_two, format = "latex", booktabs = TRUE, longtable = FALSE) %>%
   save_kable("/Users/haotianzheng/Desktop/research/Avi_Kenny/results/bias_Q_pct_two.tex")
+kable(bias_Q_pct_km, format = "latex", booktabs = TRUE, longtable = FALSE) %>%
+  save_kable("/Users/haotianzheng/Desktop/research/Avi_Kenny/results/bias_Q_pct_km.tex")
+
+kable(coverage_two, format = "latex", booktabs = TRUE, longtable = FALSE) %>%
+  save_kable("/Users/haotianzheng/Desktop/research/Avi_Kenny/results/coverage_two.tex")
+kable(coverage_km, format = "latex", booktabs = TRUE, longtable = FALSE) %>%
+  save_kable("/Users/haotianzheng/Desktop/research/Avi_Kenny/results/coverage_km.tex")
